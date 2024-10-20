2ed008ecc0266d5b3743c3d14da40f28261b0f60
hadis-aliabadi <hadis.eslami73@gmail.com>
2024-10-20 14:01:03 +0330

    Update BlogPost.js
    blog changes

--- BlogPost.js
+++ BlogPost.js
@@ -4,5 +4,5 @@ export default function BlogPost({
 }) {
   return /*#__PURE__*/React.createElement("div", {
     className: "blog-post"
-  }, /*#__PURE__*/React.createElement("h2", null, title), /*#__PURE__*/React.createElement("p", null, content));
+  }, /*#__PURE__*/React.createElement("h2", null, "hello"), /*#__PURE__*/React.createElement("p", null, "I'm here"));
 }
